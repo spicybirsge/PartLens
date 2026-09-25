d268e9f5bc20a8343961cf34eb8dc2a525edbc44
Shaheer Ahamed <92243459+spicybirsge@users.noreply.github.com>
2026-09-25 19:26:08 +0530

    ....

--- frontend/src/app/terms/page.tsx
+++ frontend/src/app/terms/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 export default async function Dashboard() {
 
-    redirect("https://github.com/spicybirsge/PartLens/blob/main/legal/privacy.md")
+    redirect("https://github.com/spicybirsge/PartLens/blob/main/legal/terms.md")
 
   return <></>;
 }
